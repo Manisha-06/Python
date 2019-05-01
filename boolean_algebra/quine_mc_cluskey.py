25d4ee728f1ad1797c5ad76aa45d272d3a7c76ab
kpf <kpf@gmail.com>
2019-05-01 19:16:26 +0530

    try commit

--- boolean_algebra/quine_mc_cluskey.py
+++ boolean_algebra/quine_mc_cluskey.py
@@ -88,7 +88,7 @@ def selection(chart, prime_implicants):
 				for j in range(len(chart)):
 					chart[j][i] = 0
 		
-def prime_implicant_chart(prime_implicants, binary):
+#def prime_implicant_chart(prime_implicants, binary):
 	chart = [[0 for x in range(len(binary))] for x in range(len(prime_implicants))]
 	for i in range(len(prime_implicants)):
 		count = prime_implicants[i].count('_')
